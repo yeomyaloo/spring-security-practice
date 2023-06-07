0a363e35173f61869c2ab90700a655f79c353378
hwiju yeom <yeom456@gmail.com>
2023-06-07 16:35:15 +0900

    Feat: Custom 필터 적용과 더 나아가 jwt 관련 Provider 작업을 진행 할 예정

--- spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/config/SecurityConfiguration.java
+++ spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.spring.security.practice.springsecuritypractice.config;
 
 
+import com.spring.security.practice.springsecuritypractice.auth.filter.CustomAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+/**
+ * 스프링 시큐리티와 관련해서 환경 설정을 진행하는 클래스입니다.
+ */
 @Configuration
 public class SecurityConfiguration {
     @Bean
@@ -14,10 +21,35 @@ public class SecurityConfiguration {
 
         http.csrf().disable();
         http.cors().disable();
-        http.formLogin().loginPage("/members/login");
+        http.formLogin()
+                .loginPage("/members/login");
 
 
+        // 관리자, 로그인할 때 볼수 있는 마이페이지 제외하고 모든 요청은 권한 없이 접속 가능
+        http.authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .antMatchers("/myPage/**", "/manager/**").authenticated();
+
+        http.addFilterAt(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    private AbstractAuthenticationProcessingFilter customAuthenticationFilter() {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter("/members/login");
+
+        customAuthenticationFilter.setAuthenticationManager();
+        customAuthenticationFilter.setAuthenticationSuccessHandler();
+        customAuthenticationFilter.setAuthenticationFailureHandler();
+
+        return customAuthenticationFilter();
+    }
+
+    /**
+     * 비밀번호 평문 저장을 방지하기 위한 엔코더 빈등록
+     * */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
