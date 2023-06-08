ba0891c70de1980ace5c43c8f5aa9445a1c61f8f
hwiju yeom <yeom456@gmail.com>
2023-06-08 14:26:29 +0900

    Feat: JPAQueryFactory 사용을 위한 빈주입 작업

---
+++ spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/config/QueryDslConfiguration.java
@@ -0,0 +1,21 @@
+package com.spring.security.practice.springsecuritypractice.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+/**
+ * query dsl에서 사용될 jpaQueryFactory 빈 등록을 위한 설정 클래스입니다.
+ * */
+public class QueryDslConfiguration {
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
