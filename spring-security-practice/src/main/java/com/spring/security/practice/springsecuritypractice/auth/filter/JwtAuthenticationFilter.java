1b6ebc1ce977d3adba097d969c272f5cee6a54ae
hwiju yeom <yeom456@gmail.com>
2023-06-13 17:55:19 +0900

    Feat: 단일 서버에서 진행되는 AuthenticationFilter 작업
    - http request 가 들어오면 해당 필터는 이 작업을 낚아서 진행한다.
    - 이때 다음 단계로 넘어갈땐 Authentication(미인증) 객체를 넘겨주는데 Authentication을 만들어서 다음 작업에 위임해준다.
    - 성공 했다면 성공했을때의 로직이 작동된다
      - 성공시엔 jwt 발급과, 상태 유지를 위한 redis에 저장 작업을 진행한다.
      - redisTemplate 설정에 객체를 넣기 위해서 설정해주었고, 이때 AuthMember라는 직렬화 객체를 넣어주야 된다. (redis는 데이터를 byte array형식으로 넣기 때문에 이를 맞춰주기 위함이다.)

--- spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/auth/filter/JwtAuthenticationFilter.java
+++ spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/auth/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,14 @@
 package com.spring.security.practice.springsecuritypractice.auth.filter;
 
+import com.spring.security.practice.springsecuritypractice.auth.AuthKeyType;
+import com.spring.security.practice.springsecuritypractice.auth.jwt.JWTProvider;
+import com.spring.security.practice.springsecuritypractice.member.domain.dto.AuthMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -10,10 +16,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static com.spring.security.practice.springsecuritypractice.auth.AuthKeyType.*;
+
+
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
+
+    private final AuthKeyType authKeyType;
+
+    private final JWTProvider jwtProvider;
+    private final RedisTemplate<String, Objects> redisTemplate;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -30,9 +48,52 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
 
 
-        super.successfulAuthentication(request, response, chain, authResult);
+        //jwt 발급
+        String accessToken = getAccessToken(auth);
+        String refreshToken = getRefreshToken(auth);
+
+        String expiredTime = jwtProvider.extractExpiredTime(accessToken).toString();
+
+        //uuid 생성 ( 쿠키에 사용, key값으로 사용 redis 저장 key : 토큰정보, 회원 역할 )
+        final String USER_UUID = UUID.randomUUID().toString();
+
+        // redis에 위의 정보 저장
+        redisTemplate.opsForHash().put(USER_UUID, ACCESS_TOKEN.getValue(), accessToken);
+        redisTemplate.opsForHash().put(USER_UUID, REFRESH_TOKEN.getValue(), refreshToken);
+
+        AuthMember authMember = new AuthMember(
+                auth,
+                accessToken,
+                expiredTime
+        );
+
+        redisTemplate.opsForHash().put(USER_UUID, JWT_TOKEN.getValue(),authMember);
+
+
+
     }
+
+    private String getAccessToken(Authentication auth) {
+        return jwtProvider.createAccessToken(
+                auth.getName(),
+                auth.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()));
+    }
+    private String getRefreshToken(Authentication auth) {
+        return jwtProvider.createRefreshToken(
+                auth.getName(),
+                auth.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()));
+    }
+
+
 }
