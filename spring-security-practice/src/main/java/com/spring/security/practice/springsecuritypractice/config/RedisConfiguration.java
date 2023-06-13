48f3445f5d7ac0239e965b792e78188871533db5
hwiju yeom <yeom456@gmail.com>
2023-06-13 17:58:26 +0900

    Feat: Redis 설정 클래스입니다. 이때 Redis 사용 시에 연산에 사용할 RedisTemplate 설정도 같이 진행했습니다.

--- spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/config/RedisConfiguration.java
+++ spring-security-practice/src/main/java/com/spring/security/practice/springsecuritypractice/config/RedisConfiguration.java
@@ -1,7 +1,24 @@
 package com.spring.security.practice.springsecuritypractice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+
+@Configuration
+@Getter
 public class RedisConfiguration {
 
     @Value("${spring.redis.host}")
@@ -15,8 +32,61 @@ public class RedisConfiguration {
     @Value("${spring.redis.database}")
     private int database;
 
-    private ClassLoader classLoader;
+
+    /**
+     * Redis Connection 설정 bean
+     *
+     * @return 레디스 설정을 한 레디스 구현체 Lettuce 입니다.
+     *
+     * */
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(this.host);
+        configuration.setPort(this.port);
+        configuration.setDatabase(this.database);
+        configuration.setPassword(this.password);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+
+        return redisTemplate;
+    }
 
 
-    
+    /**
+     * ObjectMapper에서 LocalDateTime 관련 에러가 나지 않게 설정해준 뒤 빈으로 등록해서 사용
+     *
+     * */
+    @Bean
+    public ObjectMapper objectMapper(){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+        objectMapper.registerModules(new JavaTimeModule(),
+                new Jdk8Module());
+        return objectMapper;
+
+    }
+
+
+
+
+
+
 }
